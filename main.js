d1c694bbe3b73480bf2dfc69388a4c4592e7633f
NisriinaWakhdah <ninaputri3344@gmail.com>
2026-07-28 19:55:31 +0800

    Setting up main.js

---
+++ main.js
@@ -0,0 +1,21 @@
+const {app, BrowserWindow} = require('electron');
+require('electron-reload')(__dirname) // electron-reload
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 320,
+        height: 420,
+        resizable: false,
+        fullscreenable: false,
+        center: true,
+        alwaysOnTop: true,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        }
+    });
+
+    win.loadFile('index.html');
+};
+
+app.whenReady().then(createWindow);
