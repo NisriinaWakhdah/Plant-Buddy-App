61e19d7a1b7c66cdeb5445eeeca397eaeba7a8cd
NisriinaWakhdah <ninaputri3344@gmail.com>
2026-07-29 20:02:54 +0800

    Add water meter mechanic

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Title bar buttons
 const {ipcRenderer} = require("electron");
 const minimizeBtn = document.getElementById("minimize-btn");
 const closeBtn = document.getElementById("close-btn");
@@ -7,3 +8,25 @@ minimizeBtn.addEventListener("click", () =>
 );
 
 closeBtn.addEventListener("click", () => ipcRenderer.send("window:close"));
+
+// Water meter mechanic
+const drain_amount = 1;         // to drain per tick
+const drain_interval = 900;     // 0.5% per 1s
+const water_cooldown = 10000    // 10s cooldown after watering
+
+// Default state
+let water_level = 100;
+let water_on_cooldown = false;
+let cooldown_timer = null;
+
+// Elements
+const bars = Array.from({ length: 10}, (_, i) =>
+    document.getElementById(`bar-${i + 1}`),
+);
+const percentage = document.getElementById("percentage");
+const mood_tag = document.getElementById("mood");
+const plant_icon = document.getElementById("plant");
+const care_reminder = document.getElementById("message");
+const water_btn = document.getElementById("water-btn");
+const water_timer = water_btn.querySelector(".timer");
+const restart_btn = document.getElementById("restart-btn");
